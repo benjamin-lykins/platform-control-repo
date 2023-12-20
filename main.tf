909645c4918510f1fff9686fca6130d12efd5963
Ben Lykins <benjamin.m.lykins@gmail.com>
2023-12-19 19:57:59 -0500

    adding org

--- main.tf
+++ main.tf
@@ -33,7 +33,8 @@ module "workspacer" {
 }
 
 resource "tfe_project" "this" {
-  for_each = toset(var.projects)
-  name     = each.key
+  for_each     = toset(var.projects)
+  name         = each.key
+  organization = var.organization
 
 }
