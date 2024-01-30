9e65d48e93709e828e24fb55ad94bc65d974b8fd
Ben Lykins <benjamin.m.lykins@gmail.com>
2024-01-30 11:00:04 -0500

    update module

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "workspacer" {
-  source  = "app.terraform.io/RPTData/workspacer/tfe"
+  source  = "app.terraform.io/lykins-demo-org/workspacer/tfe"
   version = "~> 1.0"
 
   for_each = var.workspaces
