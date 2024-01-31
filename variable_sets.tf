df19476d24223444aeb098e77188b132e1fd80d0
Ben Lykins <benjamin.m.lykins@gmail.com>
2024-01-31 11:44:32 -0500

    ap,projects,vs

---
+++ variable_sets.tf
@@ -0,0 +1,62 @@
+// Global Variable Sets
+
+resource "tfe_variable_set" "global_azure" {
+  name         = "azure-global-vs"
+  description  = "TJX Azure Global Variable Set."
+  organization = var.organization
+}
+
+resource "tfe_variable" "azure_dynamic_provider_auth" {
+  category        = "env"
+  key             = "TFC_AZURE_PROVIDER_AUTH"
+  value           = true
+  variable_set_id = tfe_variable_set.global_azure.id
+}
+
+resource "tfe_variable_set" "global_vsphere" {
+  name         = "vsphere-vs"
+  description  = "TJX vSphere Global Variable Set."
+  organization = var.organization
+}
+
+// Azure Subscription Variable Sets
+
+resource "tfe_variable_set" "azure_nonprod" {
+  name         = "azure-nonprod-vs"
+  description  = "TJX Azure NonProd Subscription Variable Set."
+  organization = var.organization
+}
+
+resource "tfe_variable" "tjx_subscription_name_nonprod" {
+  category        = "terraform"
+  key             = "tjx_subscription_name"
+  value           = "nonprod"
+  variable_set_id = tfe_variable_set.azure_nonprod.id
+}
+
+resource "tfe_variable_set" "azure_prod" {
+  name         = "azure-prod-vs"
+  description  = "TJX Azure NonProd Subscription Variable Set."
+  organization = var.organization
+}
+
+resource "tfe_variable" "tjx_subscription_name_prod" {
+  category        = "terraform"
+  key             = "tjx_subscription_name"
+  value           = "prod"
+  variable_set_id = tfe_variable_set.azure_prod.id
+}
+
+// vSphere Variable Sets
+
+resource "tfe_variable_set" "vsphere_ctc_nonprod" {
+  name         = "vsphere-ctc-nonprod-vs"
+  description  = "TJX vSphere CTC NonProd Variable Set."
+  organization = var.organization
+}
+
+resource "tfe_variable_set" "vsphere_ctc_prod" {
+  name         = "vsphere-ctc-nonprod-vs"
+  description  = "TJX vSphere CTC Prod Variable Set."
+  organization = var.organization
+}
--- workspaces.auto.tfvars
+++ workspaces.auto.tfvars
@@ -5,14 +5,21 @@ workspaces = {
     vcs_repo = {
       identifier = "benjamin-lykins/demo-azure-common"
     }
-    tfvars = {
-      environment = "nonprod"
-    }
-    envvars = {
-      TFC_AZURE_PROVIDER_AUTH = true
-      TFC_AZURE_RUN_CLIENT_ID = "corp-application1-nonprod-qa"
-    }
   }
 }
 
-projects = ["corp-general", "azure-shared", "vsphere-shared"]
+projects = [
+  "azure-shared-nonprod",
+  "azure-shared-prod",
+  "azure-nonprod",
+  "azure-prod",
+  "vsphere-shared",
+  "vsphere-ctc-nonprod",
+  "vsphere-ctc-prod",
+  "vsphere-ctc-vmcod-nonprod",
+  "vsphere-ctc-vmcod-prod",
+  "vsphere-slough-nonprod",
+  "vsphere-slough-prod",
+  "vsphere-slough-vmcod-nonprod",
+  "vsphere-slough-vmcod-prod"
+]
